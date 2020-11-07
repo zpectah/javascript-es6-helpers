b81bf53cf1f903ed19c64f242fb2eaf13125b3ea
Tomas Sychra <sychrat@gmail.com>
2020-11-07 14:23:21 +0100

    init

---
+++ README.md
@@ -0,0 +1,250 @@
+# Javascript ES6 helper utilities
+
+Simple method organized in objects for better importing. 
+Most of these methods have error handling.
+
+Project is still in development, so please be patient.
+
+## Import
+
+``import * as jsHelpers from 'javascript-es6-helpers';'``
+
+or
+
+``import { date, number, string, array, cookies, storage, session, element, http, document } from 'javascript-es6-helpers';'``
+
+## Method groups:
+
+
+### { date }
+
+#### date.getTodayObject() => object{}
+
+#### date.getTimestampString() => string
+
+name | type | default value
+------------ | ------------- | -------------
+separator | string
+
+
+### { number }
+
+#### number.getTwoDecimal(oneDigitNumber) => string
+
+name | type | default value
+------------ | ------------- | -------------
+number | number | 
+
+
+### { string }
+
+#### string.getRandom(length?, type?) => string
+
+name | type | default value
+------------ | ------------- | -------------
+length | number | 16
+type | string | 'all'
+
+#### string.getToken(length?, separator?) => string
+
+name | type | default value
+------------ | ------------- | -------------
+length | number | 8
+separator | string | '-'
+
+
+### { array }
+
+#### array.add(array, item) => array[]
+
+name | type | default value
+------------ | ------------- | -------------
+array | array | 
+item | any | 
+
+#### array.remove(array, index) => array[]
+
+name | type | default value
+------------ | ------------- | -------------
+array | array | 
+index | number | 
+
+
+### { cookies }
+
+#### cookies.set(name, value, days?, path?)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string |
+value | string |
+days | number | 365
+path | string | '/'
+
+#### cookies.get(name) => string|null
+
+name | type | default value
+------------ | ------------- | -------------
+name | string |
+
+#### cookies.check(name) => boolean
+
+name | type | default value
+------------ | ------------- | -------------
+name | string |
+
+#### cookies.delete(name)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string |
+
+
+## { storage }
+
+### set(name, value)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+value | string
+
+### get(name) => string
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+
+### remove(name)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+
+### clearAll()
+
+
+## { session }
+
+### set(name, value)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+value | string
+
+### get(name) => string
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+
+### remove(name)
+
+name | type | default value
+------------ | ------------- | -------------
+name | string
+
+### clearAll()
+
+
+### { element }
+
+#### element.get(selector, parent?) => node
+
+name | type | default value
+------------ | ------------- | -------------
+selector | string |
+parent | node | document
+
+#### element.getAll(selector, parent?) => array[node]
+
+name | type | default value
+------------ | ------------- | -------------
+selector | string |
+parent | node | document
+
+#### element.remove(element) => boolean
+
+name | type | default value
+------------ | ------------- | -------------
+element | node |
+
+#### element.create(tagName, content, parent?) => boolean
+
+name | type | default value
+------------ | ------------- | -------------
+tagName | string
+content | string
+parent | node | document
+
+#### element.append(parentElement, childElement) => boolean
+
+name | type | default value
+------------ | ------------- | -------------
+parentElement | node
+childElement | node
+
+#### element.addClass(element, classList)
+
+name | type | default value
+------------ | ------------- | -------------
+element | node
+classList | array
+
+#### element.removeClass(element, classList)
+
+name | type | default value
+------------ | ------------- | -------------
+element | node
+classList | array
+
+#### element.toggleClass(element, classList)
+
+name | type | default value
+------------ | ------------- | -------------
+element | node
+classList | array
+
+#### element.addAttr(element, attrsObject)
+
+name | type | default value
+------------ | ------------- | -------------
+element | node
+attrsObject | object
+
+#### element.removeAttr(element, attrsList)
+
+name | type | default value
+------------ | ------------- | -------------
+element | node
+attrsList | array
+
+
+## { http }
+
+#### http.ajax(method, url, body, headers?) => promise => object{}
+
+name | type | default value
+------------ | ------------- | -------------
+method | string
+url | string
+body | object
+headers | object | { 'Content-Type': 'application/json' }
+
+
+## { document }
+
+### onReady(callback) => callback(event)
+
+name | type | default value
+------------ | ------------- | -------------
+callback | function
+
+### beforeUnload(callback) => callback(event)
+
+name | type | default value
+------------ | ------------- | -------------
+callback | function
+
+
---
+++ index.js
@@ -0,0 +1,416 @@
+/*
+Created by: zpecter.com
+Url: https://github.com/zpectah/javascript-es6-helpers
+*/
+
+const messages = {
+    error: 'Undefined error',
+    elementNotSet: 'Element  not set!',
+    selectorNotSet: 'Selector not set!',
+    tagNotSet: 'Tag name not set!',
+    classListArray: 'Class list must be an array!',
+    notValidElement: 'Not valid node element!',
+    parentOrChildNotSet: 'Parent or child element not set!',
+    notValidArray: 'Not valid Array field!',
+    notValidObject: 'Not valid Object!'
+};
+
+export const date = {
+    getTodayObject: function () {
+        const D = new Date();
+
+        return {
+            year: D.getFullYear(),
+            month: D.getMonth() + 1,
+            day: D.getDate(),
+            hour: D.getHours(),
+            minute: D.getMinutes(),
+            second: D.getSeconds(),
+        };
+    },
+    getTimestampString: function (
+        separator = ''
+    ) {
+        const T = this.getTodayObject();
+
+        return T.year + separator +  number.getTwoDecimal( T.month ) + separator +  number.getTwoDecimal( T.day ) + separator +  number.getTwoDecimal( T.hour ) + separator +  number.getTwoDecimal( T.minute );
+    },
+};
+
+export const number = {
+    getTwoDecimal: function (
+        number
+    ) {
+        let string = String( number );
+        if ( Number( number ) <= 9 ) string = '0' + '' + number;
+
+        return String( string );
+    },
+};
+
+export const string = {
+    getRandom: function (
+        length = 16,
+        type = 'all'
+    ) {
+        const subs = {
+            upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            lower: 'abcdefghijklmnopqrstuvwxyz',
+            number: '0123456789',
+            special: '_-'
+        };
+        const get_string = () => {
+            let string = subs.upper + subs.lower + subs.number + subs.special;
+            switch ( type ) {
+                case 'uppercase':
+                    string = subs.upper;
+                    break;
+                case 'lowercase':
+                    string = subs.lower;
+                    break;
+                case 'number':
+                    string = subs.number;
+                    break;
+                case 'special':
+                    string = subs.special;
+                    break;
+            }
+            return string;
+        };
+        let text = '';
+        const possible = get_string();
+        for ( let i = 0; i < length; i++ ) {
+            text += possible.charAt( Math.floor( Math.random() * possible.length ) );
+        }
+
+        return text;
+    },
+    getToken: function (
+        length = 8,
+        separator = '-'
+    ) {
+        const subs = {
+            a: this.getRandom( ( length / 2 ), 'lowercase' ),
+            b: this.getRandom( ( length ), 'number' ),
+            c: this.getRandom( ( length ), 'uppercase' ),
+            d: this.getRandom( ( length * 2 ) ),
+        };
+
+        return subs.a + separator + subs.b + separator + subs.c + separator + subs.d;
+    },
+};
+
+export const array = {
+    add: function (
+        array,
+        item
+    ) {
+        if (Array.isArray(array)) {
+            if (array.indexOf(item) === -1) array.push();
+        } else {
+            console.warn(messages.notValidArray);
+        }
+
+        return array;
+    },
+    remove: function (
+        array,
+        index
+    ) {
+        if (Array.isArray(array)) {
+            if (index > -1) array.splice(index, 1);
+        } else {
+            console.warn(messages.notValidArray);
+        }
+
+        return array;
+    },
+};
+
+export const cookies = {
+    set: function (
+        name,
+        value,
+        days = 365,
+        path = '/'
+    ) {
+        let d = new Date();
+        d.setTime(d.getTime() + (days*24*60*60*1000));
+        let expires = 'expires='+ d.toUTCString();
+        document.cookie = name + '=' + value + ';' + expires + ';path=' + path;
+    },
+    get: function (
+        name
+    ) {
+        let cname = name + '=';
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for(let i = 0; i <ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(cname) == 0) {
+                return c.substring(cname.length, c.length);
+            }
+        }
+
+        return null;
+    },
+    check: function (
+        name
+    ) {
+        let present;
+        if (this.get(name) !== null) {
+            present = true;
+        } else {
+            present = false;
+        }
+
+        return present;
+    },
+    remove: function (
+        name
+    ) {
+        document.cookie = name + '=; Max-Age=-99999999;';
+    },
+};
+
+export const storage = {
+    set: function (
+        name,
+        value
+    ) {
+        localStorage.setItem(name, value);
+    },
+    get: function (
+        name
+    ) {
+        return localStorage.getItem(name);
+    },
+    remove: function (
+        name
+    ) {
+        localStorage.removeItem(name);
+    },
+    clearAll: function () {
+        localStorage.clear();
+    },
+};
+
+export const session = {
+    set: function (
+        name,
+        value
+    ) {
+        sessionStorage.setItem(name, value);
+    },
+    get: function (
+        name
+    ) {
+        return sessionStorage.getItem(name);
+    },
+    remove: function (
+        name
+    ) {
+        sessionStorage.removeItem(name);
+    },
+    clearAll: function () {
+        sessionStorage.clear();
+    },
+};
+
+export const element = {
+    get: function (
+        selector,
+        parent = document
+    ) {
+        if (selector) {
+            return parent.querySelector(selector);
+        } else {
+            console.warn(messages.selectorNotSet);
+
+            return null;
+        }
+    },
+    getAll: function (
+        selector,
+        parent = document
+    ) {
+        if (selector) {
+            return parent.querySelectorAll(selector);
+        } else {
+            console.warn(messages.selectorNotSet);
+
+            return null;
+        }
+    },
+    remove: function (
+        element
+    ) {
+        if (element) {
+            element.remove();
+
+            return true;
+        } else {
+            console.warn(messages.elementNotSet);
+
+            return false;
+        }
+    },
+    create: function (
+        tagName,
+        content,
+        parent = document
+    ) {
+        if (tagName) {
+            const parent = parent.createElement(tagName);
+            if (content) parent.innerHTML = content;
+
+            return parent;
+        } else {
+            console.warn(messages.tagNotSet);
+
+            return null;
+        }
+    },
+    append: function (
+        parentElement,
+        childElement
+    ) {
+        if (parentElement && childElement) {
+            if (parentElement.nodeType && childElement.nodeType) {
+                parentElement.appendChild(childElement);
+
+                return true;
+            } else {
+                console.warn(messages.notValidElement);
+
+                return false;
+            }
+        } else {
+            console.warn(messages.parentOrChildNotSet);
+
+            return false;
+        }
+    },
+    addClass: function (
+        element,
+        classList
+    ) {
+        if (element) {
+            if (Array.isArray(classList)) {
+                if (classList.length > 0) {
+                    classList.forEach(item => element.classList.add(item));
+                }
+            } else {
+                console.warn(messages.classListArray);
+            }
+        } else {
+            console.warn(messages.elementNotSet);
+        }
+    },
+    removeClass: function (
+        element,
+        classList
+    ) {
+        if (element) {
+            if (Array.isArray(classList)) {
+                if (classList.length > 0) {
+                    classList.forEach(item => element.classList.remove(item));
+                }
+            } else {
+                console.warn(messages.classListArray);
+            }
+        } else {
+            console.warn(messages.elementNotSet);
+        }
+    },
+    toggleClass: function (
+        element,
+        classList
+    ) {
+        if (element) {
+            if (Array.isArray(classList)) {
+                if (classList.length > 0) {
+                    classList.forEach(item => element.classList.toggle(item));
+                }
+            } else {
+                console.warn(messages.classListArray);
+            }
+        } else {
+            console.warn(messages.elementNotSet);
+        }
+    },
+    addAttr: function (
+        element,
+        attrsObject
+    ) {
+        if (element) {
+            if (typeof attrsObject === 'object') {
+                for (const [key, value] of Object.entries(attrsObject)) {
+                    element.setAttribute(key, value);
+                }
+            } else {
+                console.warn(messages.notValidObject);
+            }
+        } else {
+            console.warn(messages.elementNotSet);
+        }
+    },
+    removeAttr: function (
+        element,
+        attrsList
+    ) {
+        if (element) {
+            if (Array.isArray(attrsList)) {
+                if (attrsList.length > 0) {
+                    attrsList.forEach(item => element.removeAttribute(item));
+                }
+            } else {
+                console.warn(messages.classListArray);
+            }
+        } else {
+            console.warn(messages.elementNotSet);
+        }
+    },
+};
+
+export const http = {
+    ajax: function await (
+        method,
+        url,
+        body,
+        headers = {
+            'Content-Type': 'application/json'
+        }
+    ) {
+        return fetch(
+            url, {
+                method: method.toUpperCase(),
+                headers: headers,
+                body
+            }
+        )
+        .then(resp => resp.json())
+        .then(response => response)
+        .catch(error => {
+            console.warn(error);
+        });
+    }
+};
+
+export const document = {
+    onReady: function (callback) {
+        return window.addEventListener('DOMContentLoaded', (e) => {
+            if (callback && typeof callback == 'function') return callback(e);
+        });
+    },
+    beforeUnload: function (callback) {
+        return window.addEventListener('beforeunload', (e) => {
+            e.preventDefault();
+            e.returnValue = '';
+            if (callback && typeof callback == 'function') return callback(e);
+        });
+    }
+};
